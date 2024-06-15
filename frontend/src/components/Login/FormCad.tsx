b0c3cc3c4cd0b88f7fb2b152930128217018668a
rolemberg <vinnie.nsts@gmail.com>
2024-06-15 12:19:13 -0300

    created new user function

--- frontend/src/components/Login/FormCad.tsx
+++ frontend/src/components/Login/FormCad.tsx
@@ -1,4 +1,25 @@
 export default function FormCad() {
+
+   async function cadaster(e: any) {
+      e.preventDefault();
+      
+      try {
+         const [ name, email, pass ]: any = [ document.getElementById('input_name'), document.getElementById('input_email'), document.getElementById('input_password') ];
+         const url: string = 'http://localhost:8080/api/user/new/';
+
+         const req: Response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ name: name.value, email: email.value, password: pass.value }),
+            headers: new Headers({ 'content-type': 'application/json' })
+         });
+
+         if(req.status == 201) console.log('created');
+         
+      } catch(e) {
+         console.log(e);
+      }
+   };
+
    return(
       <div className=" p-5">
 
@@ -13,7 +34,10 @@ export default function FormCad() {
             <label className=" text-2xl" htmlFor="input_password">Password</label>
             <input className=" indent-2 p-1 rounded-xl border border-slate-900" type="password" name="password" id="input_password" />
 
-            <button className="self-center py-1 px-6 rounded-lg mt-5 border w-fit font-semibold border-slate-800 bg-slate-900 text-slate-50 hover:bg-slate-800">
+            <button 
+               onClick={cadaster}
+               className="self-center py-1 px-6 rounded-lg mt-5 border w-fit font-semibold border-slate-800 bg-slate-900 text-slate-50 hover:bg-slate-800"
+            >
                Enter
             </button>
 
