ece8ef2b7b22c337619b5e9759ec279d16bbc824
rolemberg <vinnie.nsts@gmail.com>
2024-06-15 12:10:02 -0300

    created function to get token login

--- frontend/src/components/Login/FormCad.tsx
+++ frontend/src/components/Login/FormCad.tsx
@@ -1,27 +1,25 @@
 export default function FormCad() {
    return(
-      <>
-         <div className=" p-5">
+      <div className=" p-5">
 
-            <form className="flex flex-col p-5 rounded-3xl shadow-lg shadow-slate-950 bg-slate-100">
+         <form className="flex flex-col p-5 rounded-3xl shadow-lg shadow-slate-950 bg-slate-100">
 
-               <label className=" text-2xl" htmlFor="input_name">Name</label>
-               <input className=" mb-5 p-1 rounded-xl border border-slate-900" type="text" name="name" id="input_name" />
+            <label className=" text-2xl" htmlFor="input_name">Name</label>
+            <input className=" indent-2 mb-5 p-1 rounded-xl border border-slate-900" type="text" name="name" id="input_name" />
 
-               <label className=" text-2xl" htmlFor="input_email">E-mail</label>
-               <input className=" mb-5 p-1 rounded-xl border border-slate-900" type="text" name="email" id="input_email" />
+            <label className=" text-2xl" htmlFor="input_email">E-mail</label>
+            <input className=" indent-2 mb-5 p-1 rounded-xl border border-slate-900" type="text" name="email" id="input_email" />
 
-               <label className=" text-2xl" htmlFor="input_password">Password</label>
-               <input className=" p-1 rounded-xl border border-slate-900" type="password" name="password" id="input_password" />
+            <label className=" text-2xl" htmlFor="input_password">Password</label>
+            <input className=" indent-2 p-1 rounded-xl border border-slate-900" type="password" name="password" id="input_password" />
 
-               <button className="self-center py-1 px-6 rounded-lg mt-5 border w-fit font-semibold border-slate-800 bg-slate-900 text-slate-50 hover:bg-slate-800">
-                  Enter
-               </button>
+            <button className="self-center py-1 px-6 rounded-lg mt-5 border w-fit font-semibold border-slate-800 bg-slate-900 text-slate-50 hover:bg-slate-800">
+               Enter
+            </button>
 
-            </form>
+         </form>
 
-         </div>
-      </>
+      </div>
    );
 };
 
--- frontend/src/components/Login/FormLogin.tsx
+++ frontend/src/components/Login/FormLogin.tsx
@@ -1,24 +1,46 @@
 export default function FormLogin() {
+
+   async function getToken(e: any) {
+      e.preventDefault();
+      
+      try {
+         const [ email, pass ]: any = [ document.getElementById('input_email'), document.getElementById('input_password') ];
+         const url = 'http://localhost:8080/api/auth/login/';
+
+         const req: Response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ email: email.value, password: pass.value }),
+            headers: new Headers({ 'content-type': 'application/json' })
+         });
+
+         const token: string = await req.text();
+         document.cookie = `token=${token}`;
+         
+      } catch(e) {
+         console.log(e);
+      }
+   };
+
    return(
-      <>
-         <div className=" p-5">
+      <div className=" p-5">
 
-            <form className="flex flex-col p-5 rounded-3xl shadow-lg shadow-slate-950 bg-slate-100">
+         <form className="flex flex-col p-5 rounded-3xl shadow-lg shadow-slate-950 bg-slate-100">
 
-               <label className=" text-2xl" htmlFor="input_email">E-mail</label>
-               <input className=" mb-5 p-1 rounded-xl border border-slate-900" type="text" name="email" id="input_email" />
+            <label className=" text-2xl" htmlFor="input_email">E-mail</label>
+            <input className=" indent-2 mb-5 p-1 rounded-xl border border-slate-900" type="text" name="email" id="input_email" />
 
-               <label className=" text-2xl" htmlFor="input_password">Password</label>
-               <input className=" p-1 rounded-xl border border-slate-900" type="password" name="password" id="input_password" />
+            <label className=" text-2xl" htmlFor="input_password">Password</label>
+            <input className=" indent-2 p-1 rounded-xl border border-slate-900" type="password" name="password" id="input_password" />
 
-               <button className="self-center py-1 px-6 rounded-lg mt-5 border w-fit font-semibold border-slate-800 bg-slate-900 text-slate-50 hover:bg-slate-800">
-                  Enter
-               </button>
+            <button 
+               onClick={getToken}
+               className="self-center py-1 px-6 rounded-lg mt-5 border w-fit font-semibold border-slate-800 bg-slate-900 text-slate-50 hover:bg-slate-800">
+               Enter
+            </button>
 
-            </form>
+         </form>
 
-         </div>
-      </>
+      </div>
    );
 };
 
