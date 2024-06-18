1c8f7e6550062a1218eda3f3f19cb3ebc6ff866f
rolemberg <vinnie.nsts@gmail.com>
2024-06-18 20:47:18 -0300

    implemented getting user func;

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,15 @@ import InferiorMenu from "./components/Generics/InferiorMenu";
 import StatusFilter from "./components/Generics/StatusFilter";
 import TodoCard from "./components/Home/TodoCard";
 import NewTodo from "./components/Todos/NewTodo";
-// import User from "./components/Interface/User/User";
+import User from "./components/Interface/User/User";
 import Todo from "./components/Interface/Todos/Todo";
+import { FaSpinner } from 'react-icons/fa6';
 
 function App() {
-   // const [ user, setUser ] = useState<User>();
+   const [ user, setUser ] = useState<User>();
    const [ todos, setTodos ] = useState<Array<Todo>>();
    const [ newTodo, setNewTodo ] = useState<string>('hidden');
+   const [ loading, setLoading ] = useState<string>();
 
    useEffect(() => {
       
@@ -39,24 +41,41 @@ function App() {
       handleGetTodos();
 
       async function handleGetUser(): Promise<void> {
-         
+
          try {
+            const token: string = document.cookie.split('Bearer=')[1].split(';')[0];
+            const uid: string = document.cookie.split('UID=')[1];
+            const url: string = `http://3.219.123.52:8080/api/user/get/${parseInt(uid)}/`;
 
-            
+            const request: Response = await fetch(url, {
+               method: 'GET',
+               headers: new Headers({ 'content-type': 'application/json', 'Authorization': `Bearer ${token}` })
+            });
 
+            const user: User = await request.json();
+
+            setUser(user);
+            setLoading('hidden');
          } catch(e) {   
             console.log(e);
          }
-
       }
+      handleGetUser();
 
    }, []);
 
    const handleNewTodo = (): void => newTodo === 'hidden' ? setNewTodo('') : setNewTodo('hidden');
 
    return(
-      <section className=" select-none">
-         <Header title={'Hello user!'} />
+      <section className="select-none">
+         <div className={`${loading} flex justify-center items-center fixed top-0 w-screen h-screen bg-slate-200`}>
+            <FaSpinner
+               size={80}
+               className={`loading_spinner`}
+            />
+         </div>
+
+         <Header title={`Hello, ${user?.name.split(' ')[0]}`} />
 
          <div className=" pt-7">
             <StatusFilter />
