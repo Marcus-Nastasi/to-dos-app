9844ef744087d4912dbeae55ecb36ef1ddd78948
rolemberg <vinnie.nsts@gmail.com>
2024-06-18 00:02:31 -0300

    implemented menu config

--- frontend/src/components/Generics/Header.tsx
+++ frontend/src/components/Generics/Header.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 import { FaEllipsisV, FaAlignJustify } from 'react-icons/fa';
 import MenuOp from './MenuOp';
+import MenuConfig from './MenuConfig';
 
 export default function Header({ title }: any) {
    const [ menuBg, setMenuBg ] = useState<string>('#353535');
    const [ pointsBg, setPointsBg ] = useState<string>('#353535');
    const [ showMenu, setShowMenu ] = useState<string>('hidden');
+   const [ showConfig, setShowConfig ] = useState<string>('hidden');
 
    const handleMenuColorIn = (): void => setMenuBg('#838383');
    const handleMenuColorOut = (): void => setMenuBg('#353535');
+   
    const handlePointsColorIn = (): void => setPointsBg('#838383');
    const handlePointsColorOut = (): void => setPointsBg('#353535');
 
-   const handleMenu = (): void => showMenu === 'hidden' ? setShowMenu('') : setShowMenu('hidden');
+   const handleMenuOp = (): void => showMenu === 'hidden' ? setShowMenu('') : setShowMenu('hidden');
+   const handleMenuConfig = (): void => showConfig === 'hidden' ? setShowConfig('') : setShowConfig('hidden');
 
    return(
       <>
@@ -22,7 +26,7 @@ export default function Header({ title }: any) {
                <FaAlignJustify
                   onMouseOver={handleMenuColorIn}
                   onMouseLeave={handleMenuColorOut}
-                  onClick={handleMenu}
+                  onClick={handleMenuOp}
                   className=' hover:cursor-pointer'
                   size={23}
                   color={menuBg}
@@ -30,7 +34,17 @@ export default function Header({ title }: any) {
 
             </div>
 
-            <MenuOp show={showMenu} func={handleMenu} />
+            <div className=' w-screen fixed top-0 flex justify-between'>
+               <MenuOp 
+                  show={showMenu} 
+                  func={handleMenuOp} 
+               />
+
+               <MenuConfig
+                  show={showConfig} 
+                  func={handleMenuConfig} 
+               />
+            </div>
 
             <div className=" pt-5">
                <h1 className=" text-3xl">
@@ -43,6 +57,7 @@ export default function Header({ title }: any) {
                <FaEllipsisV 
                   onMouseOver={handlePointsColorIn}
                   onMouseLeave={handlePointsColorOut}
+                  onClick={handleMenuConfig}
                   className=' hover:cursor-pointer'
                   size={23}
                   color={pointsBg}
---
+++ frontend/src/components/Generics/MenuConfig.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { FaX } from 'react-icons/fa6';
+
+export default function MenuConfig({ show, func }: any) {
+   const [ closeButton, setCloseButton ] = useState<string>('#353535');
+
+   const handleCloseBtnColorIn = (): void => setCloseButton('#838383');
+   const handleCloseBtnColorOut = (): void => setCloseButton('#353535');
+
+   return(
+      <div className={`${show} min-h-screen max-h-fit w-screen fixed top-0 right-0 p-10 bg-slate-100 lg:w-1/3`}>
+         <div className='flex flex-col'>
+            <div className=''>
+               <FaX
+                  onMouseOver={handleCloseBtnColorIn}
+                  onMouseLeave={handleCloseBtnColorOut}
+                  color={closeButton}
+                  onClick={func}
+                  size={22}
+                  className='hover:cursor-pointer'
+               />
+            </div>
+
+            <div className='self-center'>
+
+               <div className='flex flex-col text-2xl'>
+                  <a id='menuDashLink' className='my-2 hover:text-slate-700' href="/user/configurations">Configurations</a>
+                  <a className='hover:text-slate-700' href="/user/account">Account</a>
+               </div>
+
+            </div>
+         </div>
+      </div>
+   );
+};
+
+
