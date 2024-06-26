4fcb1b8ec5d0deb907f1fc585889df5a31ca93a6
rolemberg <vinnie.nsts@gmail.com>
2024-06-26 17:19:12 -0300

    implemented js of Account page

--- frontend/src/Account.tsx
+++ frontend/src/Account.tsx
@@ -1,30 +1,36 @@
-
+import FormEdit from "./components/Account/FormEdit";
 
 export default function Account() {
-
    return(
-      <div className=" min-h-screen max-h-fit w-screen p-10 text-slate-700 selection:select-none">
-         <div className=" w-full h-fit border p-4 flex flex-col justify-between items-center rounded-md border-neutral-300 bg-neutral-200">
-            <div className=" text-lg w-full flex justify-between items-center">
-               <p>User</p>
-               <p>username</p>
-            </div>
-
-            <div className=" text-lg w-full flex justify-between items-center">
-               <p>E-mail</p>
-               <p>email</p>
-            </div>
-
-            <div className=" text-lg w-full flex justify-between items-center">
-               <p>Password</p>
-               <p>************</p>
-            </div>
-
-            <button className="font-medium px-4 py-0.5 mt-5 rounded-md border border-neutral-600 text-slate-100 bg-neutral-500 hover:bg-neutral-400">
-               Edit
-            </button>
+      <>
+         <div className={` w-screen h-screen absolute top-0 overflow-y-scroll bg-white`}>
+            <FormEdit />
          </div>
-      </div>
+
+         <div className=" min-h-screen max-h-fit w-screen p-10 text-slate-700 selection:select-none">
+
+            <div className=" w-full h-fit border p-4 flex flex-col justify-between items-center rounded-md border-neutral-300 bg-neutral-200">
+               <div className=" text-lg w-full flex justify-between items-center">
+                  <p>User</p>
+                  <p>username</p>
+               </div>
+
+               <div className=" text-lg w-full flex justify-between items-center">
+                  <p>E-mail</p>
+                  <p>email</p>
+               </div>
+
+               <div className=" text-lg w-full flex justify-between items-center">
+                  <p>Password</p>
+                  <p>************</p>
+               </div>
+
+               <button className="font-medium px-4 py-0.5 mt-5 rounded-md border border-neutral-600 text-slate-100 bg-neutral-500 hover:bg-neutral-400">
+                  Edit
+               </button>
+            </div>
+         </div>
+      </>
    );
 };
 
---
+++ frontend/src/components/Account/FormEdit.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import ErrorBox from "../Handler/ErrorBox";
+
+export default function FormEdit() {
+   const [ error, setError ] = useState<boolean>();
+   const [ errorMessage, setErrorMessage ] = useState<string>();
+
+   async function handleUpdate(e: any): Promise<void> {
+      e.preventDefault();
+
+      const token: string = document.cookie.split('Bearer=')[1].split(';')[0];
+      const uid: string = document.cookie.split('UID=')[1];
+      const url: string = `http://192.168.0.76:8080/api/user/update/${parseInt(uid)}/`;
+
+      const [ n, em, op, np ]: any = [ document.getElementById('name'), document.getElementById('email'), document.getElementById('currentPassword'), document.getElementById('newPassword') ];
+
+      try {
+         const request: Response = await fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify({ name: n.value, email: em.value, currentPassword: op.value, newPassword: np.value }),
+            headers: new Headers({ 'content-type': 'application/json', 'Authorization': `Bearer ${token}` })
+         });
+
+         if(request.status !== 202) {
+            handleError('error while updating');
+            return
+         }
+
+         document.cookie = 'Bearer=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+         document.cookie = 'UID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+         window.open('/login/', '_self');
+      } catch(e: any) {
+         handleError(e.message);
+      }
+   };
+  
+  // Exemplo de uso
+   const handleError = (e: string): void => {
+      setErrorMessage(e);
+      setError(true);
+      setTimeout(() => setError(false), 4000);
+      return
+   };
+
+   return(
+      <>
+         <form className="h-screen w-screen flex flex-col items-center pt-20">
+
+            {error ? <ErrorBox message={errorMessage} />: ''}
+
+            <h2 className=" text-4xl mb-10">Update user</h2>
+
+            <div className="flex flex-col w-11/12 rounded-md shadow-sm shadow-neutral-500 p-4 bg-neutral-200">
+               <label className=" text-lg" htmlFor="name">Name:</label>
+               <input className="p-1 rounded-sm mb-5" type="text" name="name" id="name" />
+
+               <label className=" text-lg" htmlFor="email">E-mail:</label>
+               <input className="p-1 rounded-sm mb-5" type="text" name="email" id="email" />
+
+               <label className=" text-lg" htmlFor="currentPassword">Current password:</label>
+               <input className=" p-1 rounded-sm mb-5" type="password" name="currentPassword" id="currentPassword" />
+
+               <label className=" text-lg" htmlFor="newPassword">New password:</label>
+               <input className=" p-1 rounded-sm mb-5" type="password" name="newPassword" id="newPassword" />
+
+               <button onClick={handleUpdate} className=" self-center w-fit px-6 py-1 border rounded-md border-neutral-500 bg-neutral-300 hover:bg-neutral-400">
+                  Update
+               </button>
+            </div>
+
+         </form>
+      </>
+   );
+};
+
+
+
